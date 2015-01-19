3b860fb84c3f495c6b4d8779d5b94238b0b68c3a
Brendan Kenny <bckenny@google.com>
2015-01-19 23:53:39 +0000

    switch cube to icosahedron

--- app/elements/offsite-globe/geometry.js
+++ app/elements/offsite-globe/geometry.js
@@ -1,37 +1,74 @@
 IOWA.OffsiteGlobe.generateGeometry = (function() {
   'use strict';
 
+  // normalized coordinates of icosahedron vertices, all of which lie in the x-,
+  // y-, or z-planes.
+  var SHORT_DIM = Math.sqrt((5 - Math.sqrt(5)) / 10);
+  var LONG_DIM = Math.sqrt((5 + Math.sqrt(5)) / 10);
+
   var CORNERS = [
-    0, 1, 0, 1,
-    0, 0, 0, 1,
-    1, 1, 0, 1,
-    1, 0, 0, 1,
-    1, 1, -1, 1,
-    1, 0, -1, 1,
-    0, 1, -1, 1,
-    0, 0, -1, 1,
+    SHORT_DIM, 0, LONG_DIM, 1,
+    -SHORT_DIM, 0, LONG_DIM, 1,
+    0, LONG_DIM, SHORT_DIM, 1,
+    LONG_DIM, SHORT_DIM, 0, 1,
+    LONG_DIM, -SHORT_DIM, 0, 1,
+    0, -LONG_DIM, SHORT_DIM, 1,
+
+    0, -LONG_DIM, -SHORT_DIM, 1,
+    -LONG_DIM, -SHORT_DIM, 0, 1,
+    -LONG_DIM, SHORT_DIM, 0, 1,
+    0, LONG_DIM, -SHORT_DIM, 1,
+    SHORT_DIM, 0, -LONG_DIM, 1,
+    -SHORT_DIM, 0, -LONG_DIM, 1,
+
+    // LONG_DIM, SHORT_DIM, 0, 1,
+    // -LONG_DIM, SHORT_DIM, 0, 1,
+    // LONG_DIM, -SHORT_DIM, 0, 1,
+    // -LONG_DIM, -SHORT_DIM, 0, 1,
+
+    // SHORT_DIM, 0, LONG_DIM, 1,
+    // SHORT_DIM, 0, -LONG_DIM, 1,
+    // -SHORT_DIM, 0, LONG_DIM, 1,
+    // -SHORT_DIM, 0, -LONG_DIM, 1,
+
+    // 0, LONG_DIM, SHORT_DIM, 1,
+    // 0, -LONG_DIM, SHORT_DIM, 1,
+    // 0, LONG_DIM, -SHORT_DIM, 1,
+    // 0, -LONG_DIM, -SHORT_DIM, 1
   ];
 
   function generateGeometry() {
     return {
       geometryArray: new Float32Array(CORNERS),
       indexArray: new Uint16Array([
-        0, 1, 2,
-        2, 1, 3,
-        2, 3, 4,
-        4, 3, 5,
-        4, 5, 6,
-        6, 5, 7,
-        6, 7, 0,
-        0, 7, 1,
-
         // top
-        6, 0, 4,
-        4, 0, 2,
+        0, 2, 1,
+        0, 3, 2,
+        0, 4, 3,
+        0, 5, 4,
+        0, 1, 5,
+
+        5, 1, 7,
+        1, 8, 7,
+        1, 2, 8,
+
+        8, 2, 9,
+
+        2, 3, 9,
+        3, 10, 9,
+        3, 4, 10,
+
+        10, 4, 6,
+
+        4, 5, 6,
+        5, 7, 6,
 
         // bottom
-        1, 7, 3,
-        3, 7, 5
+        6, 7, 11,
+        7, 8, 11,
+        8, 9, 11,
+        9, 10, 11,
+        10, 6, 11
       ])
     };
   }
