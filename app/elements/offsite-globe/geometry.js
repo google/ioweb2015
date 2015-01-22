9d5dc0a932692da39dd32af543bf12143bd4a401
Brendan Kenny <bckenny@google.com>
2015-01-22 03:38:52 +0000

    simplify geometry generation

--- app/elements/offsite-globe/geometry.js
+++ app/elements/offsite-globe/geometry.js
@@ -9,8 +9,8 @@
 IOWA.OffsiteGlobe.generateIcosphere = (function() {
   'use strict';
 
-  // normalized coordinates of icosahedron vertices, all of which lie in the x-,
-  // y-, or z-planes.
+  // normalized coordinates of icosahedron vertices, all of which lie soley in
+  // the x-, y-, or z-planes.
   var SHORT_DIM = Math.sqrt((5 - Math.sqrt(5)) / 10);
   var LONG_DIM = Math.sqrt((5 + Math.sqrt(5)) / 10);
 
@@ -72,13 +72,14 @@ IOWA.OffsiteGlobe.generateIcosphere = (function() {
     divisions = Math.max(1, divisions);
 
     // 2n^2 + 3n + 1 vertices for each of five columns of four faces each
-    // don't share verts between columns as it buys very little for a lot of pain
+    // don't share verts between columns as it buys very little for much pain
     var vertCount = 5 * ((2 * divisions + 3) * divisions + 1);
     var geom = new Float32Array(vertCount * 5 * SIZE);
 
     // divisions^2 triangles per 20 faces
     var indices = new Uint16Array(20 * 3 * divisions * divisions);
 
+    // temp coordinate buffers for interpolation end points
     var left = new Float32Array(4);
     var mid = new Float32Array(4);
     var right = new Float32Array(4);
@@ -87,66 +88,56 @@ IOWA.OffsiteGlobe.generateIcosphere = (function() {
     var geomIndex = 0;
     var indicesIndex = 0;
 
+    // generate five sets of faces
     for (var i = 0; i < 5; i++) {
       var startGeomIndex = geomIndex;
 
-    // first pair of faces
-    var topLeft = 0;
-    var topRight = ((i + 1) % 5 + 1) * SIZE;
-    var bottomLeft = (i + 1) * SIZE;
-    var bottomRight = ((i + 1) % 5 + 6) * SIZE;
+      // corners of top pair of faces
+      var topLeft = 0;
+      var topRight = ((i + 1) % 5 + 1) * SIZE;
+      var bottomLeft = (i + 1) * SIZE;
+      var bottomRight = ((i + 1) % 5 + 6) * SIZE;
 
-    var v, ui, u;
-    for (var vi = 0; vi < (divisions + 1); vi++) {
-      v = vi / divisions;
-      lerp4(left, 0, CORNERS, topLeft, CORNERS, bottomLeft, v);
-      lerp4(mid, 0, CORNERS, topRight, CORNERS, bottomLeft, v);
-      lerp4(right, 0, CORNERS, topRight, CORNERS, bottomRight, v);
-
-      for (ui = 0; ui < divisions - vi; ui++) {
-        u = ui / (divisions - vi);
-        lerp4(geom, geomIndex * SIZE, left, 0, mid, 0, u);
+      // first line of verts gets its very own loop to avoid some singularities
+      for (var ui = 0; ui < divisions + 1; ui++) {
+        lerp4(geom, geomIndex * SIZE, CORNERS, topLeft, CORNERS, topRight, ui / divisions);
         geomIndex++;
       }
-      for (ui = 0; ui < vi + 1; ui++) {
-        // TODO(bckenny): not wild about this, but technically correct because
-        // parameter is undefined here. Find a way to rearrange
-        u = ui === 0 ? 0 : ui / vi;
-        lerp4(geom, geomIndex * SIZE, mid, 0, right, 0, u);
-        geomIndex++;
+
+      // two or more, use a for
+      for (var j = 0; j < 2; j++) {
+        for (var vi = 1; vi < divisions + 1; vi++) {
+          var v = vi / divisions;
+          lerp4(left, 0, CORNERS, topLeft, CORNERS, bottomLeft, v);
+          lerp4(mid, 0, CORNERS, topRight, CORNERS, bottomLeft, v);
+          lerp4(right, 0, CORNERS, topRight, CORNERS, bottomRight, v);
+
+          var u;
+          for (ui = 0; ui < divisions - vi; ui++) {
+            u = ui / (divisions - vi);
+            lerp4(geom, geomIndex * SIZE, left, 0, mid, 0, u);
+            geomIndex++;
+          }
+          for (ui = 0; ui < vi + 1; ui++) {
+            u = ui / vi;
+            lerp4(geom, geomIndex * SIZE, mid, 0, right, 0, u);
+            geomIndex++;
+          }
+        }
+
+        // switch to corners of bottom pair of faces
+        topLeft = bottomLeft;
+        topRight = bottomRight;
+        bottomLeft = (i + 6) * SIZE;
+        bottomRight = 11 * SIZE;
       }
-    }
 
-    // second pair of faces
-    topLeft = bottomLeft;
-    topRight = bottomRight;
-    bottomLeft = (i + 6) * SIZE;
-    bottomRight = 11 * SIZE;
-
-    // TODO(bckenny): skip the first row (already in place)
-    for (vi = 1; vi < (divisions + 1); vi++) {
-      v = vi / divisions;
-      lerp4(left, 0, CORNERS, topLeft, CORNERS, bottomLeft, v);
-      lerp4(mid, 0, CORNERS, topRight, CORNERS, bottomLeft, v);
-      lerp4(right, 0, CORNERS, topRight, CORNERS, bottomRight, v);
-
-      for (ui = 0; ui < divisions - vi; ui++) {
-        u = ui / (divisions - vi);
-        lerp4(geom, geomIndex * SIZE, left, 0, mid, 0, u);
-        geomIndex++;
-      }
-      for (ui = 0; ui < vi + 1; ui++) {
-        // don't need guard here since skip the first vi
-        u = ui / vi;
-        lerp4(geom, geomIndex * SIZE, mid, 0, right, 0, u);
-        geomIndex++;
-      }
-    }
-
-    indicesIndex = generateGridIndices(indices, indicesIndex,
-        startGeomIndex, divisions);
-    indicesIndex = generateGridIndices(indices, indicesIndex,
-        startGeomIndex + divisions * (divisions + 1), divisions);
+      // TODO(bckenny): simplify this since completely determined by divisions
+      // add indices of top and bottom vertices to index array
+      indicesIndex = generateGridIndices(indices, indicesIndex,
+          startGeomIndex, divisions);
+      indicesIndex = generateGridIndices(indices, indicesIndex,
+          startGeomIndex + divisions * (divisions + 1), divisions);
     }
 
     return {
