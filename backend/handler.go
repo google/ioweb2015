f917ad6c653265b7f9133e95db7bb9245fd3bc68
alex <alex@cloudware.io>
2015-02-06 16:48:29 +0000

    Cache-control headers:
    * defaults to 1 day
    * js, css and elements.html for 30 min
    * templated pages including /home (landing page) for 5 min
    * /api/{extended,social} for 1 min
    
    Closes #495

--- backend/handler.go
+++ backend/handler.go
@@ -61,6 +61,7 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 		data.OgImage = ogImageExperiment
 	}
 	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	w.Header().Set("Cache-Control", "public, max-age=300")
 	err := renderTemplate(c, tplname, wantsPartial, data)
 
 	if err != nil {
@@ -76,6 +77,7 @@ func serveIOExtEntries(w http.ResponseWriter, r *http.Request) {
 	_, refresh := r.Form["refresh"]
 
 	c := newContext(r, w)
+	w.Header().Set("Cache-Control", "public, max-age=60")
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 
 	// respond with stubbed JSON entries in dev mode
@@ -111,6 +113,7 @@ func serveSocial(w http.ResponseWriter, r *http.Request) {
 	_, refresh := r.Form["refresh"]
 
 	c := newContext(r, w)
+	w.Header().Set("Cache-Control", "public, max-age=60")
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 
 	// respond with stubbed JSON entries in dev mode
