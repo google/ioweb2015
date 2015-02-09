ceb31622bd00487a661bd429d2daeef11f7afea0
alexmahan <alexmahan@gmail.com>
2015-02-09 13:25:35 -0800

    Fixes #425.

--- experiment/loader/index.js
+++ experiment/loader/index.js
@@ -66,6 +66,7 @@ window.experiment = (function() {
       }, timeoutDuration);
 
       var scr = document.createElement('script');
+      scr.addEventListener('error', rejector);
       scr.setAttribute('async', 'true');
       scr.type = 'text/javascript';
       scr.src = assetPath('/js/experiment.js');
