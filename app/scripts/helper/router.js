624ed34c753e0e4cc7ccddeba72efccc36f76972
Ewa Gasperowicz <devnook@gmail.com>
2015-02-10 15:32:17 +0000

    Adding path

--- app/scripts/helper/router.js
+++ app/scripts/helper/router.js
@@ -418,7 +418,7 @@ IOWA.Router = (function() {
       }
       document.title = (IOWA.Elements.Template.pages[nextPage].title ||
           'Google I/O 2015');
-      IOWA.Analytics.trackPageView();
+      IOWA.Analytics.trackPageView(e.state.path);
       renderCurrentPage();
     });
 
