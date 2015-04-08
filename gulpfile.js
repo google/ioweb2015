b80486da95cf1496bd82c6587391f4ca8fbb1a64
alex <alex@cloudware.io>
2015-04-09 00:05:22 +0100

    Also deploy temporary_api/json files

--- gulpfile.js
+++ gulpfile.js
@@ -151,6 +151,7 @@ gulp.task('copy-assets', function() {
     APP_DIR + '/elements/webgl-globe/textures/*.{jpg,png}',
     APP_DIR + '/bower_components/webcomponentsjs/webcomponents.min.js',
     APP_DIR + '/bower_components/es6-promise-2.0.1.min/index.js',
+    APP_DIR + '/temporary_api/*.json',
     DIST_EXPERIMENT_DIR + '/**/*'
   ], {base: './'});
 
