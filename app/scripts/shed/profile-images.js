7493ce9e578c9344133412c87e6d1f4763be82e9
Jeff Posnick <jeffy@google.com>
2015-04-13 13:10:01 -0400

    Caching for speaker profile images.

---
+++ app/scripts/shed/profile-images.js
@@ -0,0 +1,11 @@
+var DEFAULT_PROFILE_IMAGE_URL = 'images/touch/homescreen96.png';
+
+function profileImageRequest(request) {
+  return shed.cacheFirst(request).catch(function() {
+    return shed.cacheOnly(new Request(DEFAULT_PROFILE_IMAGE_URL));
+  });
+}
+
+shed.precache([DEFAULT_PROFILE_IMAGE_URL]);
+shed.router.get('/(.+)/images/speakers/(.*)', profileImageRequest,
+  {origin: /https?:\/\/storage\.googleapis\.com/});
