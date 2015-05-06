42b45f9db6b39180640e4e828e4dcbe61c255621
Eric Bidelman <ebidel@gmail.com>
2015-05-06 15:50:23 -0700

    Update meta them on each page

--- app/scripts/helper/router.js
+++ app/scripts/helper/router.js
@@ -246,6 +246,11 @@ IOWA.Router = (function() {
     if (this.state.current.page !== this.state.start.page) {
       document.body.id = 'page-' + pageName;
       document.title = pageMeta.title || 'Google I/O 2015';
+      var metaTheme = document.documentElement.querySelector('meta[name="theme-color"]')
+      if (metaTheme) {
+        metaTheme.content = IOWA.Elements.Template.rippleColors[pageMeta.mastheadBgClass];
+      }
+
       // This cannot be updated via data binding, because the masthead
       // is visible before the binding happens.
       IOWA.Elements.Masthead.className = IOWA.Elements.Masthead.className.replace(
